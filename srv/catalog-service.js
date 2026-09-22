4b5616579321740b90d825af9038228e0954620f
jakes2255 <aj727in@gmail.com>
2026-09-22 19:18:12 +0530

    features updates-4

--- srv/catalog-service.js
+++ srv/catalog-service.js
@@ -3,9 +3,19 @@ const cds = require('@sap/cds')
 module.exports = cds.service.impl(async function () {
 
     const northwind = await cds.connect.to('northwind_metadata')
+    const localProducts = [
+        { ProductID: 1, ProductName: 'Chai', SupplierID: 1, CategoryID: 1, QuantityPerUnit: '10 boxes x 20 bags', UnitPrice: 18, UnitsInStock: 39, UnitsOnOrder: 0, ReorderLevel: 10, Discontinued: false },
+        { ProductID: 2, ProductName: 'Chang', SupplierID: 1, CategoryID: 1, QuantityPerUnit: '24 - 12 oz bottles', UnitPrice: 19, UnitsInStock: 17, UnitsOnOrder: 40, ReorderLevel: 25, Discontinued: false },
+        { ProductID: 3, ProductName: 'Aniseed Syrup', SupplierID: 1, CategoryID: 2, QuantityPerUnit: '12 - 550 ml bottles', UnitPrice: 10, UnitsInStock: 13, UnitsOnOrder: 70, ReorderLevel: 25, Discontinued: false }
+    ]
 
     this.on('READ', 'Products', async (req) => {
-        return northwind.run(req.query)
+        try {
+            return await northwind.run(req.query)
+        } catch (error) {
+            if (!cds.env.profiles.includes('development')) throw error
+            return localProducts
+        }
     })
 
 })
